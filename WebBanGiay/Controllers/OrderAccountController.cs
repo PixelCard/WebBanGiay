aa940d6f676ae6564bf30087c1dbcf7b2532aa55
Thanh Toàn <140700994+PixelCard@users.noreply.github.com>
2025-07-20 19:14:58 +0700

    add layout order và chỉnh sửa các header cho các trang

---
+++ WebBanGiay/Controllers/OrderAccountController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanGiay.Models;
+using System.Data.Entity;
+
+namespace WebBanGiay.Controllers
+{
+    public class OrderAccountController : Controller
+    {
+        private CNPM_LTEntities db = new CNPM_LTEntities();
+
+        // GET: OrderAccount
+        public ActionResult OrderAccountPage()
+        {
+            // Kiểm tra xem người dùng đã đăng nhập chưa
+            if (Session["Email"] == null)
+            {
+                return RedirectToAction("LoginPage", "Account");
+            }
+
+            string userEmail = Session["Email"].ToString();
+            
+            // Tìm tài khoản của người dùng
+            var account = db.Accounts.FirstOrDefault(a => a.Email == userEmail);
+            if (account == null)
+            {
+                return RedirectToAction("LoginPage", "Account");
+            }
+
+            // Tìm khách hàng tương ứng
+            var customer = db.Customers.FirstOrDefault(c => c.IDAccount == account.IDTK);
+            if (customer == null)
+            {
+                return RedirectToAction("LoginPage", "Account");
+            }
+
+            // Lấy danh sách đơn hàng của khách hàng với các navigation properties
+            var orders = db.Orders
+                .Include(o => o.PaymentMethod)
+                .Include(o => o.OrderDetails)
+                .Include(o => o.OrderDetails.Select(od => od.ProductVariant))
+                .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Product))
+                .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Size))
+                .Include(o => o.OrderDetails.Select(od => od.ProductVariant.Color))
+                .Where(o => o.CustomerID == customer.CustomerID)
+                .OrderByDescending(o => o.OrderDate)
+                .ToList();
+
+            ViewBag.Customer = customer;
+            ViewBag.Orders = orders;
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
